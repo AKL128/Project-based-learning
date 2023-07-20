dd42991084881cec4e699fa2437d02b72bb592b0
akl128 <anthonyluu128@gmail.com>
2023-07-20 05:58:13 -0500

    Arguments parsed into a list of integers

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
+using System.Collections;
 namespace mathApp
 {
     class Program
     {
-        public static void Main(int[] args)
+        public static void Main(string[] args)
         {
-            
+        string input;
+        List<int> numberList = new List<int>();
+
+        foreach (string arg in args)
+        {
+            if (int.TryParse(arg, out int number))
+            {
+                numberList.Add(number);
+            }
+        }
+        
+
+
         }
     }
 }
